b9bcf3bcf4a463005744714862982a97b61a5398
Mohit Srivastava <mohitsrivastava67@gmail.com>
2022-03-30 17:00:49 +0530

    added: notes menu on the notes page

--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -8,6 +8,7 @@ import notesApi from "apis/notes";
 import EmptyState from "components/Common/EmptyState";
 
 import DeleteAlert from "./DeleteAlert";
+import NotesMenu from "./MenuBar";
 import NewNotePane from "./Pane/Create";
 import Table from "./Table";
 
@@ -15,6 +16,7 @@ const Notes = () => {
   const [loading, setLoading] = useState(true);
   const [showNewNotePane, setShowNewNotePane] = useState(false);
   const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedNoteIds, setSelectedNoteIds] = useState([]);
   const [notes, setNotes] = useState([]);
@@ -40,52 +42,55 @@ const Notes = () => {
   }
 
   return (
-    <Container>
-      <Header
-        title="All Notes"
-        menuBarToggle={() => {}}
-        actionBlock={
-          <Button
-            onClick={() => setShowNewNotePane(true)}
-            label="Add New Note"
-            icon="ri-add-line"
+    <>
+      <NotesMenu showMenu={showMenu} />
+      <Container>
+        <Header
+          title="All Notes"
+          menuBarToggle={() => setShowMenu(!showMenu)}
+          actionBlock={
+            <Button
+              onClick={() => setShowNewNotePane(true)}
+              label="Add New Note"
+              icon="ri-add-line"
+            />
+          }
+          searchProps={{
+            value: searchTerm,
+            onChange: e => setSearchTerm(e.target.value),
+          }}
+        />
+        {notes.length ? (
+          <Table
+            notes={notes}
+            fetchNotes={fetchNotes}
+            setSelectedNoteIds={setSelectedNoteIds}
+            setShowDeleteAlert={setShowDeleteAlert}
           />
-        }
-        searchProps={{
-          value: searchTerm,
-          onChange: e => setSearchTerm(e.target.value),
-        }}
-      />
-      {notes.length ? (
-        <Table
-          notes={notes}
+        ) : (
+          <EmptyState
+            image={EmptyNotesListImage}
+            title="Looks like you don't have any notes!"
+            subtitle="Add your notes to send customized emails to them."
+            primaryAction={() => setShowNewNotePane(true)}
+            primaryActionLabel="Add New Note"
+          />
+        )}
+        <NewNotePane
+          showPane={showNewNotePane}
+          setShowPane={setShowNewNotePane}
           fetchNotes={fetchNotes}
-          setSelectedNoteIds={setSelectedNoteIds}
-          setShowDeleteAlert={setShowDeleteAlert}
         />
-      ) : (
-        <EmptyState
-          image={EmptyNotesListImage}
-          title="Looks like you don't have any notes!"
-          subtitle="Add your notes to send customized emails to them."
-          primaryAction={() => setShowNewNotePane(true)}
-          primaryActionLabel="Add New Note"
-        />
-      )}
-      <NewNotePane
-        showPane={showNewNotePane}
-        setShowPane={setShowNewNotePane}
-        fetchNotes={fetchNotes}
-      />
-      {showDeleteAlert && (
-        <DeleteAlert
-          selectedNoteIds={selectedNoteIds}
-          onClose={() => setShowDeleteAlert(false)}
-          refetch={fetchNotes}
-          setSelectedNoteIds={setSelectedNoteIds}
-        />
-      )}
-    </Container>
+        {showDeleteAlert && (
+          <DeleteAlert
+            selectedNoteIds={selectedNoteIds}
+            onClose={() => setShowDeleteAlert(false)}
+            refetch={fetchNotes}
+            setSelectedNoteIds={setSelectedNoteIds}
+          />
+        )}
+      </Container>
+    </>
   );
 };
 
